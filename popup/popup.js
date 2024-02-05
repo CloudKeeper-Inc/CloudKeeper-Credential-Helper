7d8bf00183305781eea0e4a33168d7d62d738541
Aditya Ajay <aditya.ajay@tothenew.com>
2024-02-05 10:38:11 +0530

    feat: copy button for credentials

--- popup/popup.js
+++ popup/popup.js
@@ -12,3 +12,36 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   browser.storage.sync.clear();
 });
+
+// Function to copy text to clipboard and update button text
+function copyToClipboardAndUpdateButton(elementId, buttonId) {
+  var copyText = document.getElementById(elementId);
+  var textArea = document.createElement("textarea");
+  textArea.value = copyText.innerText;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+
+  // Update button text to "Copied!" with animation
+  var button = document.getElementById(buttonId);
+  button.innerText = "Copied!";
+  button.classList.add("copied-animation");
+
+  // Reset button text after 2 seconds
+  setTimeout(function() {
+    button.innerText = "Copy Credentials";
+    button.classList.remove("copied-animation");
+  }, 2000);
+}
+
+// Attach click event listeners using JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("copyCredentialsButton").addEventListener("click", function() {
+    copyToClipboardAndUpdateButton('credentialsFile', 'copyCredentialsButton');
+  });
+
+  document.getElementById("copyEnvVariablesButton").addEventListener("click", function() {
+    copyToClipboardAndUpdateButton('env_variables', 'copyEnvVariablesButton');
+  });
+});
