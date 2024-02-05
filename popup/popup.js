999a0eb5bc642422c7be48c3176d568cde183330
Aditya Ajay <aditya.ajay@tothenew.com>
2024-02-05 10:26:50 +0530

    firefox branch

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,14 @@
 // On load of popup
 document.addEventListener("DOMContentLoaded", function () {
-  chrome.storage.sync.get(["credentialsFile"], function (data) {
+  browser.storage.sync.get(["credentialsFile"], function (data) {
     if (typeof data.credentialsFile !== "undefined")
       document.getElementById("credentialsFile").innerText =
         data.credentialsFile;
   });
-  chrome.storage.sync.get(["env_variables"], function (data) {
+  browser.storage.sync.get(["env_variables"], function (data) {
     if (typeof data.env_variables !== "undefined")
       document.getElementById("env_variables").innerText =
         data.env_variables;
   });
-  chrome.storage.sync.clear();
+  browser.storage.sync.clear();
 });
